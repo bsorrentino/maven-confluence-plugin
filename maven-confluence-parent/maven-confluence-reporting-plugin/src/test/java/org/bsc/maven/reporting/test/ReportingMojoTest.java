1d91cdfa1e51203ce2d31884c20b6d7e23bf4dd4
bartolomeo.sorrentino <bartolomeo.sorrentino@6a545194-3771-11de-8a2e-13aa1706fec1>
2009-05-02 23:55:33 +0000

    initial import
    git-svn-id: https://maven-confluence-plugin.googlecode.com/svn/trunk@9 6a545194-3771-11de-8a2e-13aa1706fec1

---
+++ maven-confluence-parent/maven-confluence-reporting-plugin/src/test/java/org/bsc/maven/reporting/test/ReportingMojoTest.java
@@ -0,0 +1,38 @@
+package org.bsc.maven.reporting.test;
+import java.io.File;
+
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.bsc.maven.reporting.ConfluenceReportMojo;
+import org.codehaus.plexus.PlexusContainer;
+
+
+
+public class ReportingMojoTest extends AbstractMojoTestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testLookup() throws Exception  {
+/*		
+		File testPom = new File( getBasedir(), "/src/test/resources/test-pom.xml" );
+
+		PlexusContainer container = createContainerInstance();
+		
+        assertNotNull( "plexus container is null", container);
+		
+		Mojo mojo = (Mojo) lookupMojo ("confluence-summary", testPom );
+
+        assertNotNull( "mojo is null", mojo);
+        assertTrue( mojo instanceof ConfluenceReportMojo);
+        
+        ConfluenceReportMojo confluenceMojo = (ConfluenceReportMojo) mojo;
+        
+        
+        System.out.println( confluenceMojo.getSink() );
+*/        
+	}
+	
+}
