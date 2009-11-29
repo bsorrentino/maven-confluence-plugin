9161cda5ee2af247ccab9ac14b2db4d92eeab76c
bartolomeo.sorrentino <bartolomeo.sorrentino@6a545194-3771-11de-8a2e-13aa1706fec1>
2009-11-29 16:06:09 +0000

    git-svn-id: https://maven-confluence-plugin.googlecode.com/svn/tags/maven-confluence-parent-1.0@153 6a545194-3771-11de-8a2e-13aa1706fec1

--- maven-confluence-reporting-plugin-plugin/maven-doc.bat
+++
@@ -1,1 +0,0 @@
-mvn -e clean compile -Dconfluence.spaceKey=SFTFACT  -Dconfluence.userName=bsorrentino -Dconfluence.password=BactiuHS  -Dconfluence.parentPage="Maven Plugins" -Dconfluence.endPoint=https://asia.herzumsoftware.com/confluence/rpc/xmlrpc confluence:plugin-doc
