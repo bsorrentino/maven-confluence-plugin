db80447d08d5674f0abbbf9f3767f6d90d893452
bartolomeo.sorrentino <bartolomeo.sorrentino@6a545194-3771-11de-8a2e-13aa1706fec1>
2009-11-29 16:11:58 +0000

    git-svn-id: https://maven-confluence-plugin.googlecode.com/svn/tags/maven-confluence-parent-1.0@155 6a545194-3771-11de-8a2e-13aa1706fec1

---
+++ maven-confluence-reporting-plugin-plugin/maven-doc.bat
@@ -0,0 +1,1 @@
+mvn -e clean compile -Dconfluence.spaceKey=SFTFACT  -Dconfluence.userName=bsorrentino -Dconfluence.password=BactiuHS  -Dconfluence.parentPage="Maven Plugins" -Dconfluence.endPoint=https://asia.herzumsoftware.com/confluence/rpc/xmlrpc confluence:plugin-doc
---
+++ maven-confluence-reporting-plugin-plugin/src/main/java/org/bsc/maven/plugin/confluence/ConfluenceWikiWriter.java
@@ -0,0 +1,122 @@
+package org.bsc.maven.plugin.confluence;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+/**
+ * 
+ * @author Sorrentino
+ *
+ */
+public class ConfluenceWikiWriter extends PrintWriter {
+
+	
+	
+	public ConfluenceWikiWriter(File file) throws FileNotFoundException {
+		super(file);
+	}
+
+	/**
+	 * 
+	 * @param out
+	 */
+	public ConfluenceWikiWriter(Writer out) {
+		super(out);
+	}
+
+	public void printBiggestHeading( String value ) {
+		super.printf( "h1. %s\n", value );
+	}
+
+	public ConfluenceWikiWriter appendBiggerHeading() {
+		append( "h2. ");
+		return this;
+	}
+	public ConfluenceWikiWriter appendBigHeading() {
+		append( "h3. ");
+		return this;
+	}
+	
+	/**
+	 *  {info:title=Useful Information}
+	 *	This macro is useful for including helpful information in your confluence pages
+	 *	{info}
+	 *
+	 * @param value
+	 */
+	public void printInfo( String title, String value ) {
+		printf( "{info:title=%s}\n%s\n{info}", title, value );
+		println();
+	}
+	
+	public void printBiggerHeading( String value ) {
+		super.printf( "h2. %s\n", value );
+	}
+
+	public void printBigHeading( String value ) {
+		super.printf( "h3. %s\n", value );
+	}
+	
+	public void printNormalHeading( String value ) {
+		super.printf( "h4. %s\n", value );
+	}
+
+	public void printSmallHeading( String value ) {
+		super.printf( "h5. %s\n", value );
+	}
+
+	public void printSmallestHeading( String value ) {
+		super.printf( "h6. %s\n", value );
+	}
+	
+	public ConfluenceWikiWriter appendBullet() {
+		append("* ");
+		return this;	
+	}
+	
+	public void printBullet( String value ) {
+		printBullets(1, value);
+	}
+	
+	public void printQuote( String value  ) {
+		println( "{quote}");
+		println( value);
+		println( "{quote}");
+	}
+	
+	public void printNewParagraph() {
+		println();
+	}
+	
+	public void printLineBreak() {
+		println( "\\\\");
+	}
+	
+	public void printStrong( String value  ) {
+		super.printf( "*%s*\n", value );		
+	}
+
+	
+	public void printBullets( int depth, String value ) {
+		for( int i=0; i< depth ; ++i ) {
+			append('*');
+		}
+		append( ' ').append(value).append('\n');
+	}
+	
+	public void printLinkToAnchor( String anchorName, String value  ) {
+		printf( "[%s|#%s]\n", value, anchorName );		
+	}
+	
+	public ConfluenceWikiWriter appendAnchor(String anchorName, String value ) {
+		printAnchor( anchorName, value );
+		return this;
+	}
+	
+	public void printAnchor( String anchorName, String value ) {
+		printf( "{anchor:%s}%s", anchorName, value );
+	}
+	
+}
