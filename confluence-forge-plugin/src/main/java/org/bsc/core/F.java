5a8ba8db869f43db22ffde2ffeb3f37c9bf77ea3
bsorrentino <bartolomeo.sorrentino@gmail.com>
2015-03-19 22:23:54 +0100

    remove forge module

--- confluence-forge-plugin/src/main/java/org/bsc/core/F.java
+++
@@ -1,6 +0,0 @@
-package org.bsc.core;
-
-public interface F<P,R> {
-    
-    R f( P param );
-};
--- confluence-forge-plugin/src/test/java/org/confluence/forge/plugin/AbstractTestCase.java
+++
@@ -1,5 +0,0 @@
-package org.confluence.forge.plugin;
-
-public abstract class AbstractTestCase
-{
-}
