b57580b7e912b25e7cbec6f885d511bdd79bce93
bsorrentino <bartolomeo.sorrentino@gmail.com>
2016-07-08 22:49:32 +0200

    update

---
+++ maven-confluence-core/src/test/java/org/bsc/maven/plugin/confluence/Issue106IntegrationTest.java
@@ -0,0 +1,155 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bsc.maven.plugin.confluence;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.concurrent.TimeUnit;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import okhttp3.Credentials;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNull;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+/**
+ *
+ * @author softphone
+ */
+public class Issue106IntegrationTest {
+    
+    OkHttpClient.Builder client;
+    
+    private int responseCount(Response response) {
+        int result = 1;
+        while ((response = response.priorResponse()) != null) {
+            result++;
+        }
+        return result;
+    }
+
+    @Before
+    public void open() {
+         client = new OkHttpClient.Builder(); 
+ 
+         /*
+         client.authenticator( new Authenticator() {
+             @Override
+             public Request authenticate(Route route, Response response) throws IOException {
+                System.out.println( "AUTHENTICATOR" );
+
+                if (responseCount(response) >= 3) {
+                    return null; // If we've failed 3 times, give up. - in real life, never give up!!
+                }
+                
+                final String credential = Credentials.basic("admin", "admin");
+                
+                return response.request()
+                                .newBuilder()
+                                .header("Authorization", credential)
+                                .build();
+                 
+             }
+         });
+         */
+         
+         client.connectTimeout(10, TimeUnit.SECONDS);
+         client.writeTimeout(10, TimeUnit.SECONDS);
+         client.readTimeout(30, TimeUnit.SECONDS);
+
+    }
+    
+    @Test
+    public void getStorageFormat() throws IOException {
+        
+        final String credential = Credentials.basic("admin", "admin");
+
+        final HttpUrl url =   new HttpUrl.Builder()
+                                    .scheme("http")
+                                    .host("192.168.99.100")
+                                    .port(8090)
+                                    .addPathSegments("plugins/viewstorage/viewpagestorage.action")
+                                    .addQueryParameter("pageId", "1867778")
+                                    .build();
+        final Request req = new Request.Builder()
+                .header("Authorization", credential)
+                .url( url )  
+                .get()
+                .build();
+        
+        final Response res = client.build().newCall(req).execute();
+        
+        Assert.assertThat( res, IsNull.notNullValue());
+        Assert.assertThat( res.isSuccessful(), Is.is(true));
+        final ResponseBody body = res.body();
+        Assert.assertThat( body, IsNull.notNullValue());
+
+        System.out.printf( "BODY\n%s\n", body.string() );
+        
+    }
+    
+    @Test
+    public void findPage() throws IOException {
+
+        final String credential = Credentials.basic("admin", "admin");
+
+        final HttpUrl url =   new HttpUrl.Builder()
+                                    .scheme("http")
+                                    .host("192.168.99.100")
+                                    .port(8090)
+                                    .addPathSegments("rest/api/")
+                                    .addPathSegment("content")                
+                                    .addQueryParameter("spaceKey", "TEST")
+                                    .addQueryParameter("title", "Home")
+                                    .build();
+        final Request req = new Request.Builder()
+                .header("Authorization", credential)
+                .url( url )  
+                .get()
+                .build();
+        
+        final Response res = client.build().newCall(req).execute();
+        
+        Assert.assertThat( res, IsNull.notNullValue());
+        Assert.assertThat( res.isSuccessful(), Is.is(true));
+        final ResponseBody body = res.body();
+        Assert.assertThat( body, IsNull.notNullValue());
+
+        try( Reader r = body.charStream()) {
+            
+            final JsonReader rdr = Json.createReader(r);
+            
+            final JsonObject root = rdr.readObject();
+            
+            Assert.assertThat( root,  IsNull.notNullValue() );
+            Assert.assertThat( root.containsKey("results"), Is.is(true) );
+            
+            final JsonArray results = root.getJsonArray("results");
+            Assert.assertThat( results,  IsNull.notNullValue() );
+            Assert.assertThat( results.size(),  IsEqual.equalTo(1) );
+
+            final JsonObject item0 = results.getJsonObject(0);
+            Assert.assertThat( item0,  IsNull.notNullValue() );
+            Assert.assertThat( item0.containsKey("id"), Is.is(true) );
+            Assert.assertThat( item0.getString("id"), IsEqual.equalTo("1867778") );
+            
+        }
+        
+    }
+    
+}
