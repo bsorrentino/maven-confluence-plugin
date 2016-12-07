c9967ffde43fd46c609145e5618baa53aafc08fc
bsorrentino <bartolomeo.sorrentino@gmail.com>
2016-12-07 02:03:12 +0100

    finalize traverse xml nodes using rx

--- dist/test-site.js
+++ dist/test-site.js
@@ -14,11 +14,39 @@ function removeSingleArrays(obj, filter) {
         }
     });
 }
-var readFile = Rx.Observable.fromNodeCallback(filesystem.readFile);
-var parseString = Rx.Observable.fromNodeCallback(parser.parseString);
-readFile(path.join(__dirname, 'site.xml'))
-    .flatMap(function (value) { return parseString(value.toString()); })
+function rxTraverse(obj) {
+    return Rx.Observable.create(function (observer) {
+        traverse(obj).forEach(function traversing(value) {
+            observer.onNext(value);
+        });
+        observer.onCompleted();
+    });
+}
+function rxProcessChild(child) {
+    if (!child || child.length == 0)
+        return Rx.Observable.empty();
+    var first = child[0];
+    var childObservable = Rx.Observable.just(first);
+    var attachmentsObservable = Rx.Observable.fromArray(first['attachment'] || []);
+    var childrenObservable = Rx.Observable.fromArray(first['child'] || [])
+        .concatMap(function (value) {
+        var o1 = Rx.Observable.just(value);
+        var o2 = Rx.Observable.fromArray(value['attachment'] || []);
+        var o3 = rxProcessChild(value['child'] || []);
+        return Rx.Observable.concat(o1, o2, o3);
+    });
+    return Rx.Observable.concat(childObservable, attachmentsObservable, childrenObservable);
+}
+var rxReadFile = Rx.Observable.fromNodeCallback(filesystem.readFile);
+var rxParseString = Rx.Observable.fromNodeCallback(parser.parseString);
+rxReadFile(path.join(__dirname, 'site.xml'))
+    .flatMap(function (value) { return rxParseString(value.toString()); })
     .doOnCompleted(function () { return console.log('Done'); })
+    .map(function (value) {
+    for (var first in value)
+        return value[first]['home'];
+})
+    .flatMap(function (value) { return rxProcessChild(value); })
     .subscribe(function (data) {
-    console.dir(data, { depth: 8 });
+    console.log("element", data['$']['name'] || data['$']['uri']);
 });
--- test-site.ts
+++ test-site.ts
@@ -20,14 +20,63 @@ function removeSingleArrays(obj, filter?:(key:string) => boolean ) {
   });
 }
 
-let readFile    = Rx.Observable.fromNodeCallback( filesystem.readFile );
-let parseString = Rx.Observable.fromNodeCallback( parser.parseString );
+function rxTraverse( obj:Object ):Rx.Observable<Object> {
 
-readFile( path.join(__dirname,'site.xml') )
-  .flatMap( (value:Buffer) => parseString( value.toString() ) )
+    return Rx.Observable.create( (observer) => {
+
+      traverse(obj).forEach(function traversing(value) {
+
+        observer.onNext( value );
+        // As the XML parser returns single fields as arrays.
+        /*
+        if (value instanceof Array && value.length === 1) {
+          if( filter && !filter(this.key) ) return;
+            this.update(value[0]);
+        }
+        */
+      });
+      observer.onCompleted();
+    });
+
+}
+
+
+function rxProcessChild( child:Array<Object> ) {
+  if( !child || child.length == 0 ) return Rx.Observable.empty();
+
+  let first = child[0];
+
+  let childObservable = Rx.Observable.just(first);
+  let attachmentsObservable = Rx.Observable.fromArray( first['attachment'] || []);
+
+  let childrenObservable = Rx.Observable.fromArray( first['child'] || [] )
+        .concatMap( value => {
+          let o1 = Rx.Observable.just(value);
+          let o2 = Rx.Observable.fromArray(value['attachment'] || []);
+          let o3 = rxProcessChild(value['child'] || []);
+          return Rx.Observable.concat( o1, o2, o3  );
+        });
+
+
+  return Rx.Observable.concat( childObservable,
+                               attachmentsObservable,
+                               childrenObservable,
+                             );
+}
+
+let rxReadFile    = Rx.Observable.fromNodeCallback( filesystem.readFile );
+let rxParseString = Rx.Observable.fromNodeCallback( parser.parseString );
+
+
+rxReadFile( path.join(__dirname,'site.xml') )
+  .flatMap( (value:Buffer) => rxParseString( value.toString() ) )
   .doOnCompleted( () => console.log('Done') )
-  .subscribe( (data:Object) => {
-    console.dir(data, {depth:8});
+  .map( (value:Object) => {
+    for( let first in value ) return value[first]['home'];
+  })
+  .flatMap( (value:Array<Object>) => rxProcessChild(value) )
+  .subscribe( (data) => {
+    console.log( "element", data['$']['name'] || data['$']['uri']);
   });
 
 /*
