d8a18807648963e908c3fbc00c9b480f1e8fa34a
bsorrentino <bartolomeo.sorrentino@gmail.com>
2016-12-19 00:01:52 +0100

    updated

--- dist/test-site.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var confluence_site_1 = require("./confluence-site");
-var path = require("path");
-confluence_site_1.rxSite(path.join(__dirname, 'site.xml'))
-    .subscribe(function (data) {
-    console.log("element", data.$.name || data.$.uri, data.type);
-});
