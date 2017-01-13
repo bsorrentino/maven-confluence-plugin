6ff213496724e0fbdf78f832b8f628c23edafec0
bsorrentino <bartolomeo.sorrentino@gmail.com>
2017-01-13 19:45:47 +0100

    remove useless ts definition

--- dist/index.js
+++ dist/index.js
@@ -64,14 +64,3 @@ function main() {
         console.dir(result, { depth: 2 });
     });
 }
-function test() {
-    var config = require("./config.json").local;
-    confluence_xmlrpc_1.XMLRPCConfluenceService.create(config, config.credentials)
-        .then(function (confluence) {
-        var site = new confluence_site_1.SiteProcessor(confluence, config.spaceId, config.pageTitle, __dirname);
-        site.rxStart('site.1.xml')
-            .doOnCompleted(function () { return confluence.connection.logout().then(function () { return console.log("logged out!"); }); })
-            .subscribe(function (elem) {
-        });
-    });
-}
