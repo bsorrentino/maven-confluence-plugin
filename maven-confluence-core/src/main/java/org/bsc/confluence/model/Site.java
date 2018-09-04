1480fda8bf3add5446b60dbce623deb73ba7615c
bsorrentino <bartolomeo.sorrentino@gmail.com>
2018-09-04 18:27:30 +0200

    issue #170 - remove the processor methods from Site to SiteProcessor remove the print methods from Site to SitePrinter refactor usage

--- maven-confluence-core/src/main/java/org/bsc/confluence/model/Site.java
+++ maven-confluence-core/src/main/java/org/bsc/confluence/model/Site.java
@@ -5,16 +5,12 @@
 package org.bsc.confluence.model;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.function.BiFunction;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -23,14 +19,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
-
-import org.apache.commons.io.IOUtils;
-import org.bsc.confluence.ConfluenceService.Storage;
-import org.bsc.functional.Tuple2;
-import org.bsc.markdown.ToConfluenceSerializer;
-import org.pegdown.PegDownProcessor;
-import org.pegdown.ast.Node;
-import org.pegdown.ast.RootNode;
 
 /**
  *
@@ -72,232 +60,6 @@ public class Site {
      */
     public void setSpaceKey(String spaceKey) {
         this.spaceKey = spaceKey;
-    }
-
-    /**
-     * 
-     * @param is
-     * @return
-     */
-    static java.io.InputStream processMarkdown(final java.io.InputStream is, final String homePageTitle)
-            throws IOException {
-
-        final char[] contents = IOUtils.toCharArray(is);
-
-        final PegDownProcessor p = new PegDownProcessor(ToConfluenceSerializer.extensions());
-
-        final RootNode root = p.parseMarkdown(contents);
-
-        ToConfluenceSerializer ser = new ToConfluenceSerializer() {
-
-            @Override
-            protected void notImplementedYet(Node node) {
-
-                final int lc[] = ToConfluenceSerializer.lineAndColFromNode(new String(contents), node);
-                throw new UnsupportedOperationException(String.format("Node [%s] not supported yet. line=[%d] col=[%d]",
-                        node.getClass().getSimpleName(), lc[0], lc[1]));
-            }
-
-            @Override
-            protected String getHomePageTitle() {
-                return homePageTitle;
-            }
-
-        };
-
-        root.accept(ser);
-
-        return new java.io.ByteArrayInputStream(ser.toString().getBytes());
-    }
-
-    /**
-     *
-     * @param uri
-     * @return
-     * @throws Exception
-     */
-    public <T> T processUriContent(final java.net.URI uri, final String homePageTitle,
-            final BiFunction<java.io.InputStream, Storage.Representation, T> onSuccess)
-            throws /* ProcessUri */Exception {
-        Objects.requireNonNull(uri, "uri is null!");
-
-        String scheme = uri.getScheme();
-
-        Objects.requireNonNull(scheme, String.format("uri [%s] is invalid!", String.valueOf(uri)));
-
-        final String source = uri.getRawSchemeSpecificPart();
-
-        final String path = uri.getRawPath();
-
-        final boolean isMarkdown = (path != null && path.endsWith(".md"));
-        final boolean isStorage = (path != null && (path.endsWith(".xml") || path.endsWith(".xhtml")));
-
-        final Storage.Representation representation = (isStorage) ? Storage.Representation.STORAGE
-                : Storage.Representation.WIKI;
-
-        java.io.InputStream result = null;
-
-        if ("classpath".equalsIgnoreCase(scheme)) {
-            ClassLoader cl = Thread.currentThread().getContextClassLoader();
-
-            result = cl.getResourceAsStream(source);
-
-            if (result == null) {
-                // getLog().warn(String.format("resource [%s] doesn't exist in context
-                // classloader", source));
-
-                cl = Site.class.getClassLoader();
-
-                final java.io.InputStream is = cl.getResourceAsStream(source);
-
-                result = (isMarkdown) ? processMarkdown(is, homePageTitle) : is;
-
-                if (result == null) {
-                    throw new Exception(String.format("resource [%s] doesn't exist in classloader", source));
-                }
-
-            }
-
-        } else {
-
-            try {
-
-                java.net.URL url = uri.toURL();
-
-                final java.io.InputStream is = url.openStream();
-
-                result = (isMarkdown) ? processMarkdown(is, homePageTitle) : is;
-
-            } catch (IOException e) {
-                throw new Exception(String.format("error opening url [%s]!", source), e);
-            }
-        }
-
-        return onSuccess.apply(result, representation);
-    }
-
-    /**
-     * 
-     * @param uri
-     * @param onSuccess
-     * @return
-     */
-    public <T> T processUri(final java.net.URI uri, java.util.function.BiFunction<Optional<Exception>,Optional<java.io.InputStream>, T> callback) {
-        Objects.requireNonNull(uri, "uri is null!");
-        Objects.requireNonNull(callback, "callback is null!");
-
-        final String scheme = uri.getScheme();
-
-        Objects.requireNonNull(scheme, String.format("uri [%s] is invalid!", String.valueOf(uri)));
-        
-        final String source = uri.getRawSchemeSpecificPart();
-
-        java.io.InputStream result = null;
-
-        if ("classpath".equalsIgnoreCase(scheme)) {
-            ClassLoader cl = Thread.currentThread().getContextClassLoader();
-
-            result = cl.getResourceAsStream(source);
-
-            if (result == null) {
-
-                cl = Site.class.getClassLoader();
-
-                result = cl.getResourceAsStream(source);
-
-                final Exception ex = new Exception(String.format("resource [%s] doesn't exist in classloader", source));
-                return callback.apply( Optional.of(ex), Optional.empty());
-
-            }
-
-        } else {
-
-            try {
-                
-                java.net.URL url = uri.toURL();
-
-                result = url.openStream();
-
-            } catch (IOException e) {
-                final Exception ex = new Exception(String.format("error opening url [%s]!", source), e);
-                return callback.apply( Optional.of(ex), Optional.empty());
-            }
-        }
-
-        return callback.apply( Optional.empty(), Optional.of(result));
-    }
-
-    /**
-     *
-     * @param uri
-     * @return
-     * @throws Exception
-     */
-    public <T> T processPageUri(final java.net.URI uri, final String homePageTitle,
-            final BiFunction<Optional<Exception>, Tuple2<Optional<java.io.InputStream>, Storage.Representation>, T> callback)
-    {
-        Objects.requireNonNull(uri, "uri is null!");
-
-        String scheme = uri.getScheme();
-
-        Objects.requireNonNull(scheme, String.format("uri [%s] is invalid!", String.valueOf(uri)));
-
-        final String source = uri.getRawSchemeSpecificPart();
-
-        final String path = uri.getRawPath();
-
-        final boolean isMarkdown = (path != null && path.endsWith(".md"));
-        final boolean isStorage = (path != null && (path.endsWith(".xml") || path.endsWith(".xhtml")));
-
-        final Storage.Representation representation = (isStorage) ? Storage.Representation.STORAGE
-                : Storage.Representation.WIKI;
-
-        java.io.InputStream result = null;
-
-        if ("classpath".equalsIgnoreCase(scheme)) {
-            ClassLoader cl = Thread.currentThread().getContextClassLoader();
-
-            result = cl.getResourceAsStream(source);
-
-            if (result == null) {
-                // getLog().warn(String.format("resource [%s] doesn't exist in context
-                // classloader", source));
-
-                cl = Site.class.getClassLoader();
-
-                final java.io.InputStream is = cl.getResourceAsStream(source);
-
-                try {
-                    result = (isMarkdown) ? processMarkdown(is, homePageTitle) : is;
-                    if (result == null) {
-                        final Exception ex = new Exception(String.format("page [%s] doesn't exist in classloader", source));
-                        return callback.apply( Optional.of(ex), Tuple2.of(Optional.empty(), representation) );
-                    }
-                } catch (IOException e) {
-                    final Exception ex = new Exception(String.format("error processing markdown for page [%s] ", source));
-                    return callback.apply( Optional.of(ex), Tuple2.of(Optional.empty(), representation) );
-                }
-
-
-            }
-
-        } else {
-
-            try {
-
-                java.net.URL url = uri.toURL();
-
-                final java.io.InputStream is = url.openStream();
-
-                result = (isMarkdown) ? processMarkdown(is, homePageTitle) : is;
-
-            } catch (IOException e) {
-                final Exception ex = new Exception(String.format("error opening/processing page [%s]!", source), e);
-                return callback.apply( Optional.of(ex), Tuple2.of(Optional.empty(), representation) );
-            }
-        }
-
-        return callback.apply( Optional.empty(), Tuple2.of(Optional.of(result), representation));
     }
 
     /**
@@ -351,12 +113,6 @@ public class Site {
         public String toString() {
             return getClass().getSimpleName() + ": " + getName() + " - " + String.valueOf(getUri());
         }
-
-        protected void validateSource() {
-            if (null == uri) {
-                throw new IllegalStateException("uri is null");
-            }
-        }
     }
 
     /**
@@ -407,6 +163,13 @@ public class Site {
             this.version = version;
         }
 
+        public Attachment() {
+
+            this.contentType = DEFAULT_CONTENT_TYPE;
+            this.version = DEFAULT_VERSION;
+        }
+        
+        /*
         public boolean hasBeenUpdatedFrom(java.util.Date date) {
             if (date != null) {
 
@@ -423,12 +186,8 @@ public class Site {
 
             return true;
         }
+        */
 
-        public Attachment() {
-
-            this.contentType = DEFAULT_CONTENT_TYPE;
-            this.version = DEFAULT_VERSION;
-        }
     }
 
     /**
@@ -441,9 +200,11 @@ public class Site {
 
         @Deprecated
         public File getSource() {
-            validateSource();
 
             final java.net.URI _uri = super.getUri();
+            if (null == _uri) {
+                throw new IllegalStateException("uri is null");
+            }
 
             if (!_uri.isAbsolute() && !"file".equals(_uri.getScheme())) {
                 throw new IllegalArgumentException("uri not represent a file");
@@ -710,63 +471,4 @@ public class Site {
     }
 
     
-    private String getPrintableStringForResource( final Source source ) {
-        return getPrintableStringForResource(source.getUri());
-    }
-    
-    private String getPrintableStringForResource( final java.net.URI uri ) {
-        
-        try {
-            Path p = Paths.get( uri );
-            return getBasedir().relativize(p).toString();
-            
-        } catch (Exception e) {
-            return uri.toString();
-        }
-        
-    }
-
-    private void printSource(PrintStream out, int level, char c, final Source source) {
-        for (int i = 0; i < level; ++i) {
-            System.out.print(c);
-        }
-        out.print(" ");
-        out.println(getPrintableStringForResource(source));
-        
-        //out.println();
-        //out.print( state.getBasedir() ); out.print( " - " ); out.print( source );
-        //out.println();
-    }
-
-    private void printChildren(PrintStream out, int level, Page parent) {
-        printSource(out, level, '-', parent);
-
-        for (Attachment attach : parent.getAttachments()) {
-
-            printSource(out, level + 1, '#', attach);
-
-        }
-        for (Page child : parent.getChildren()) {
-
-            printChildren(out, level + 1, child);
-
-        }
-    }
-
-    public void print(PrintStream out) {
-
-        out.println("Site");
-
-        if (!getLabels().isEmpty()) {
-            out.println(" Labels");
-            for (String label : getLabels()) {
-
-                out.printf("  %s\n", label);
-
-            }
-        }
-
-        printChildren(out, 0, getHome());
-
-    }
 }
