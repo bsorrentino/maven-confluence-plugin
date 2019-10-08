aebd1a76016e8a7b63eed936c0212748ae1cf569
bsorrentino <bartolomeo.sorrentino@gmail.com>
2019-10-08 05:08:01 -0500

    add retry

--- maven-confluence-core/src/main/java/org/bsc/confluence/ConfluenceService.java
+++ maven-confluence-core/src/main/java/org/bsc/confluence/ConfluenceService.java
@@ -10,9 +10,13 @@ import static java.lang.String.format;
 import java.io.Closeable;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 import java.util.regex.Pattern;
 
 import org.bsc.functional.Tuple2;
+
+import lombok.val;
 /**
  *
  * @author bsorrentino
@@ -206,7 +210,7 @@ public interface ConfluenceService extends Closeable{
                 .thenApply( parent -> 
                     parent.orElseThrow( () -> 
                         new RuntimeException( 
-                                String.format("cannot find parent page [%s] in space [%s]", parentPageTitle))) )
+                                String.format("cannot find parent page [%s] in space [%s]", parentPageTitle, spaceKey))) )
                 .thenCombine( getPage(spaceKey, title), Tuple2::of)
                 .thenCompose( tuple -> {
                     return ( tuple.getValue2().isPresent() ) ?
@@ -216,4 +220,40 @@ public interface ConfluenceService extends Closeable{
                 ;
         }
 
+    /**
+     * 
+     * @param <T>
+     * @param times
+     * @param delay
+     * @param timeUnit
+     * @param resultHandler
+     * @param action
+     * @return
+     * @see https://gist.github.com/gitplaneta/5065bbba980b2858a55f
+     */
+    default <T> CompletableFuture<T> retry( int times, 
+                                            int delay, 
+                                            TimeUnit timeUnit,
+                                            Optional<CompletableFuture<T>> resultHandler,
+                                            Supplier<CompletableFuture<T>> action) {
+        val future = resultHandler.orElseGet( () -> new CompletableFuture<>() );
+        action.get()
+                .thenAccept(a -> future.complete(a))
+                .exceptionally(ex -> {
+                    if (times <= 0) {
+                        future.completeExceptionally(ex);
+                    } else {
+                            try {
+                                timeUnit.sleep(delay);
+                                retry( times - 1, delay, timeUnit, Optional.of(future), action);       
+                            } catch (InterruptedException e) {
+                                future.completeExceptionally(e);
+                            }
+                    }
+                    return null;
+                });
+
+        return future;
+    }  
+    
   }
