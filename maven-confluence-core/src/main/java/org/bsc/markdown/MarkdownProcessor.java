d1d33b07b8d76e3be116611a80d425ea7f6134b1
bartolomeo sorrentino <bartolomeo.sorrentino@gmail.com>
2020-04-23 16:27:32 +0200

    issue #154 - remove deprecated method

--- maven-confluence-core/src/main/java/org/bsc/markdown/MarkdownProcessor.java
+++ maven-confluence-core/src/main/java/org/bsc/markdown/MarkdownProcessor.java
@@ -1,6 +1,5 @@
 package org.bsc.markdown;
 
-import org.apache.commons.io.IOUtils;
 import org.bsc.confluence.ConfluenceService;
 import org.bsc.confluence.model.Site;
 
@@ -17,7 +16,6 @@ public interface MarkdownProcessor {
     String getName();
 
     /**
-     * use alternative version below
      *
      * @param site
      * @param child
@@ -27,21 +25,6 @@ public interface MarkdownProcessor {
      * @return
      * @throws IOException
      */
-    @Deprecated
-    default java.io.InputStream processMarkdown(
-            final Site site,
-            final Site.Page child,
-            final Optional<ConfluenceService.Model.Page> page,
-            final java.io.InputStream content,
-            final String homePageTitle) throws IOException {
-
-        final char[] chars = IOUtils.toCharArray(content);
-
-        final String result =  processMarkdown( site, child, page, String.valueOf(chars), homePageTitle);
-
-        return new java.io.ByteArrayInputStream( result.getBytes() );
-    }
-
     String processMarkdown(
             final Site site,
             final Site.Page child,
