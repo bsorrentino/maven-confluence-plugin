d295ff7ce7d2d23750d4fa7f47a335ff5b7835a4
bartolomeo sorrentino <bartolomeo.sorrentino@gmail.com>
2020-06-15 23:48:10 +0200

    issue #219 - allow blog pubblication from the maven cli

--- maven-confluence-reporting-plugin/src/main/java/org/bsc/mojo/ConfluenceBlogpostMojo.java
+++ maven-confluence-reporting-plugin/src/main/java/org/bsc/mojo/ConfluenceBlogpostMojo.java
@@ -67,34 +67,23 @@ public class ConfluenceBlogpostMojo extends AbstractBaseConfluenceMojo {
         }
     }
 
-    /**
-     * ScrollVersions addon configuration. Below the template
-     *
-     * <pre>
-     * &lt;blogInfo>
-     *  &lt;title>blog title</title>  &lt;!-- mandatory -->
-     *  &lt;content>path of blog content</content>  &lt;!-- mandatory -->
-     *  &lt;version>version number</title>  &lt;!-- optional -->
-     * &lt;/blogInfo>
-     * </pre>
-     *
-     */
-    @Parameter( name = "blogInfo")
-    private BlogInputInfo blogInfo;
+    @Parameter( property="blog.title" )
+    private String title;
+
+    @Parameter( property="blog.content" )
+    private File content;
+
+    @Parameter( property="blog.version", defaultValue = "0")
+    private int version;
 
     @Override
     public void execute(ConfluenceService confluenceService) throws Exception {
 
-        if(blogInfo==null) throw new IllegalArgumentException( "blogInfo cannot be null!. Check configuration");
-        if(blogInfo.getTitle()==null) throw new IllegalArgumentException( "blogInfo.title cannot be null!. Check configuration");
-
-        final File content = blogInfo.getContent();
+        if(title==null) throw new IllegalArgumentException( "blogTitle cannot be null!. Check configuration");
 
         if(content==null) throw new IllegalArgumentException( "blogInfo.content cannot be null!. Check configuration");
-        if( !content.exists() ) throw new IllegalArgumentException( format("blogInfo.content[%s] doesn't exists!. Check configuration", content) );
-        if( !content.isFile() ) throw new IllegalArgumentException( format("blogInfo.content[%s] isn't a file!. Check configuration", content) );
-
-        getLog().debug( String.valueOf(blogInfo) );
+        if( !content.exists() ) throw new IllegalArgumentException( format("blogContent [%s] doesn't exists!. Check configuration", content) );
+        if( !content.isFile() ) throw new IllegalArgumentException( format("blogContent [%s] isn't a file!. Check configuration", content) );
 
         final Path path = content.toPath();
 
@@ -112,7 +101,7 @@ public class ConfluenceBlogpostMojo extends AbstractBaseConfluenceMojo {
                                         : contentData,
                                     representation);
 
-        final Model.Blogpost blogpost = confluenceService.createBlogpost( getSpaceKey(), blogInfo.getTitle(), storage, blogInfo.getVersion());
+        final Model.Blogpost blogpost = confluenceService.createBlogpost( getSpaceKey(), title, storage, version);
 
         confluenceService.addBlogpost( blogpost ).thenAccept( result -> {
             getLog().info( format("Blog [%s] succesfully posted!", result.getTitle()) );
