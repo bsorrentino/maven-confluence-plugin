4c8884165a91489b5dfadece4e46acf88aeede1c
bsorrentino <bartolomeo.sorrentino@gmail.com>
2020-11-27 18:01:17 +0100

    skip escape markdown text for {toc} tests

---
+++ processor-commonmark/src/test/kotlin/org/bsc/makdown/commonmark/MacroTest.kt
@@ -0,0 +1,29 @@
+package org.bsc.makdown.commonmark
+
+import org.bsc.confluence.model.Site
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
+class MacroTest {
+
+    var site = Site().apply {
+        basedir = Paths.get(System.getProperty("user.dir"))
+    }
+
+    private fun parse():String? = parseResource( this.javaClass, "macro", this.site )
+
+    @Test
+    //@Ignore
+    fun `parse macros`()  = Assertions.assertEquals(
+            """
+            h1. This is table of content
+            {toc}
+            
+            {toc:minLevel=2}
+            
+            {toc:type=flat|separator=pipe:minLevel=2}
+            """.trimIndent(),
+            parse() )
+    
+}
---
+++ processor-commonmark/src/test/resources/macro.md
@@ -0,0 +1,8 @@
+# This is table of content  
+    
+{toc}
+
+{toc:minLevel=2}
+
+{toc:type=flat|separator=pipe:minLevel=2}
+
