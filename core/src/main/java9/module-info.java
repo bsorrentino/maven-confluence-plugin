72c75d41960e7c569a1cf0043b5a9a9a6e7a39f6
bsorrentino <bartolomeo.sorrentino@gmail.com>
2020-12-13 22:20:43 +0100

    issue #224 - Removed jaxb-xml-impl in favour of jackson-xml-impl

--- core/src/main/java9/module-info.java
+++ core/src/main/java9/module-info.java
@@ -14,6 +14,7 @@ module maven.confluence.core {
 
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.dataformat.yaml;
+    requires com.fasterxml.jackson.dataformat.xml;
 
     requires java.logging;
     requires java.xml.bind;
