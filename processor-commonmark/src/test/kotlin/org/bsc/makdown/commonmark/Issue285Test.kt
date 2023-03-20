04f6530c2e567155ada95a9806e83d4b29144f1c
Build Pipeline <build.pipeline@softphone.it>
2023-03-20 19:47:02 +0100

    test: refactor test
    #285

--- processor-commonmark/src/test/kotlin/org/bsc/makdown/commonmark/Issue285Test.kt
+++ processor-commonmark/src/test/kotlin/org/bsc/makdown/commonmark/Issue285Test.kt
@@ -1,7 +1,8 @@
 package org.bsc.makdown.commonmark
 
 import org.bsc.confluence.model.Site
-import org.bsc.markdown.MarkdownVisitorHelper.isConfluenceMacro
+import org.bsc.markdown.MarkdownVisitorHelper.isConfluenceMacroOrVariable
+import org.bsc.markdown.MarkdownVisitorHelper.parseConfluenceMacro
 import org.bsc.markdown.commonmark.CommonmarkConfluenceWikiVisitor.parseHTMLComment
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -28,13 +29,70 @@ class Issue285Test {
         assertTrue( multiLineMatcher.matches() )
         assertEquals( 2, multiLineMatcher.groupCount() )
 
+        assertEquals(  "",  multiLineMatcher.group(1).trimEnd() )
+
         val content = multiLineMatcher.group(2).trimEnd()
         assertEquals( """
             {excerpt:title=MyExcerpt}
             This is the content that I want to use as my excerpt.
             {excerpt}
             """.trimIndent(), content )
-        assertTrue( isConfluenceMacro( content ), "$content\n It is not recognized as confluence macro!" )
+        assertTrue( isConfluenceMacroOrVariable( content ), "$content\n It is not recognized as confluence macro!" )
+
+    }
+
+    @Test
+    fun `parse excerpt macros with markdown content`() {
+
+        val excerptMacro = """
+            <!-- {excerpt:title=MyExcerpt}
+            # Title
+            This is the content that I **want** to use as my excerpt.
+            {excerpt} -->
+        """.trimIndent()
+
+        val multiLineMatcher = parseHTMLComment(excerptMacro)
+
+        assertTrue( multiLineMatcher.matches() )
+        assertEquals( 2, multiLineMatcher.groupCount() )
+
+        assertEquals(  "",  multiLineMatcher.group(1).trimEnd() )
+
+        val content = multiLineMatcher.group(2).trimEnd()
+
+        assertEquals( """
+            {excerpt:title=MyExcerpt}
+            # Title
+            This is the content that I **want** to use as my excerpt.
+            {excerpt}
+            """.trimIndent(), content )
+        assertTrue( isConfluenceMacroOrVariable( content ), "$content\n It is not recognized as confluence macro!" )
+
+        val m = parseConfluenceMacro(content)
+
+        assertTrue( m.matches() )
+        assertEquals( 3, m.groupCount())
+        assertEquals( """
+            
+            # Title
+            This is the content that I **want** to use as my excerpt.
+            
+            """.trimIndent(), m.group(2))
+
+
+        val mdcontent = parseContent( site, m.group(2) )
+
+        assertEquals( """
+            h1. Title
+            This is the content that I *want* to use as my excerpt.
+            """.trimIndent(), mdcontent )
+
+        assertEquals( """
+            {excerpt:title=MyExcerpt}
+            h1. Title
+            This is the content that I *want* to use as my excerpt.
+            {excerpt}
+            """.trimIndent(), "${m.group(1)}\n$mdcontent\n${m.group(3)}" )
 
     }
 
